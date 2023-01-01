ef700dc3b1d7277780cce1f701c84f84a453f34a
Joao Bezerra <Joao.Bezerra@ctw.bmwgroup.com>
2023-01-01 16:46:45 +0000

    feat(project-1): applying prettier new configs

--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -1,13 +1,10 @@
 const {queryString} = require('./queryString')
 
 describe('Object to queryString', () => {
-    it ('Should create a valid query string when an object is passed', () => {
+    it('Should create a valid query string when an object is passed', () => {
         const obj = {
-            name: 'Fabio',
-            profession: 'developer'
+            name: 'Fabio', profession: 'developer'
         }
-        expect(queryString(obj)).toBe(
-            'name=Fabio&profession=developer'
-        );
+        expect(queryString(obj)).toBe('name=Fabio&profession=developer')
     })
 })
